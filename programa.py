7469990c4033b0f6cbe1a108ac075cf5939d276e
GrayCentipede <maucarrui@gmail.com>
2018-08-14 13:02:42 -0500

    Regresa cadena

--- programa.py
+++
@@ -1,4 +0,0 @@
-def quita_espacios(cadena):
-	return [char for char in cadena if not char.isspace()]
-
-print(quita_espacios('Hola  '))
--- programas.py
+++ programas.py
@@ -1,4 +1,2 @@
 def quita_espacios(cadena):
-	return [char for char in cadena if not char.isspace()]
-
-print(quita_espacios('Hola  '))
+	return ''.join([char for char in cadena if not char.isspace()])
